8bea5a14a7c8decabc308d7639fb76bddca34dce
FiodarM <fyodormorozko95@gmail.com>
2015-04-20 15:14:43 +0300

    Initial Commit

--- lab5.py
+++ lab5.py
@@ -1,1 +1,14 @@
 __author__ = 'fiodar'
+
+from boundary_problem import *
+import matplotlib.pyplot as plt
+
+p = lambda x: 1. / x
+q = lambda x: -3 * x
+f = lambda x: -5. / x
+a, b = 1, 3
+conditions = ([5., -4., -2.], [3., 1., 0.])
+y = linear_2nd_order((p, q), f, (a, b), conditions)
+
+plt.plot(np.linspace(a, b), y)
+plt.show()
