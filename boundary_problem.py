279033c502e5d5d74e245a18c9f043afe0c62f88
FiodarM <fyodormorozko95@gmail.com>
2015-04-20 19:07:31 +0300

    RC1

--- boundary_problem.py
+++ boundary_problem.py
@@ -3,33 +3,33 @@ __author__ = 'fiodar'
 import numpy as np
 
 
-def tridiag_solve(diags, f, k0, l0):
-
-    alpha, beta, gamma = diags
+def tridiag_solve(diags, f, conditions):
+    a, b, c = diags
+    alpha, beta = conditions
     x = np.zeros_like(f)
     k, l = np.zeros_like(f), np.zeros_like(f)
-    k[0] = k0
-    l[0] = l0
+
+    k[0] = (alpha[2] - c[0] / c[1] * f[1]) / (a[0] - c[0] / c[1] * a[1])
+    l[0] = (c[0] / c[1] - b[0]) / (a[0] - c[0] / c[1] * a[1])
+
     for i in xrange(1, len(f) - 1):
-        k[i] = (f[i] - alpha[i]*k[i-1])/(alpha[i]*l[i-1] + beta[i])
-        l[i] = - gamma[i] / (alpha[i]*l[i-1] + beta[i])
+        k[i] = (f[i] - a[i] * k[i - 1]) / (a[i] * l[i - 1] + b[i])
+        l[i] = - c[i] / (a[i] * l[i - 1] + b[i])
 
-    x[-1] = (f[-1] - alpha[-1]*k[-2]) / (alpha[-1]*l[-2] + beta[-1])
-    for i in xrange(1, len(f) - 2):
-        x[-2-i] = (f[-2-i] - alpha[-2-i]*k[-3-i] - gamma[-2-i]*x[-1-i]) /\
-                  (alpha[-2-i]*l[-3-i] + beta[-2-i])
+    x[-1] = (beta[2] - a[-1] * (k[-2] + l[-2] * k[-1]) - b[-1] * k[-1]) / \
+            (a[-1] * l[-2] * l[-1] + b[-1] * l[-1] + c[-1])
 
-    x[0] = k[0] + l[0] * x[1]
+    for i in xrange(1, len(f) - 1):
+        x[-2 - i] = k[-2 - i] + l[-2 - i] * x[-1 - i]
 
     return x
 
 
-def linear_2nd_order(coefs, f, bounds, conditions, n=50):
+def linear_2nd_order(coefs, bounds, conditions, n=50, f=lambda x: 0 * x):
     p, q = coefs
-    alpha, beta = conditions
     x = np.linspace(bounds[0], bounds[1], n)
     h = x[1] - x[0]
-
+    alpha, beta = conditions
     a = [alpha[0] / 2 - alpha[1] / h, 0.5 * beta[1] / h]
     b = [alpha[0] / 2 + alpha[1], -2 * beta[1] / h]
     c = [- 0.5 * alpha[1] / h, beta[0] + 0.5 * beta[1] / h]
@@ -40,9 +40,6 @@ def linear_2nd_order(coefs, f, bounds, conditions, n=50):
     c = np.insert(c, 1, 1 / h * (1 / h + 0.5 * p(x[1:-1])))
     d = np.insert(d, 1, f(x[1:-1]))
 
-
-    k0 = 1 / (a[0] - c[0] / c[1] * a[1]) * f(x[1]) * (alpha[2] - c[0] / c[1] * f(x[1]))
-    l0 = c[0] / c[1] - b[0]
-    y = tridiag_solve((a, b, c), d, k0, l0)
+    y = tridiag_solve((a, b, c), d, conditions)
 
     return y
