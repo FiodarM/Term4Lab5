58b34cf1aec56eec1b32217cff49d7c61bfdca72
FiodarM <fyodormorozko95@gmail.com>
2015-04-21 16:23:02 +0300

    RC1

--- lab5.py
+++ lab5.py
@@ -4,11 +4,12 @@ from boundary_problem import *
 import matplotlib.pyplot as plt
 
 p = lambda x: 1. / x
-q = lambda x: -3. * x
-f = lambda x: -5. / x
-a, b, n = 1., 3., 100000
+q = lambda x: 1 - 0 / x ** 2
+f = lambda x: -0. / x
+a, b, n = 0., 3., 100
 conditions = ([5., -4., -2.], [3., 1., 0.])
-y = linear_2nd_order((p, q), (a, b), conditions, n, f)
+simple_conditions = ([1., 0, 0.], [1, 0, 1.])
+y = linear_2nd_order((p, q), (a, b), simple_conditions, n, f)
 
 plt.plot(np.linspace(a, b, n), y)
 plt.show()
