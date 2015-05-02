b6b72a53f4d63b75fbae07bb313391c26f78771a
FiodarM <fyodormorozko95@gmail.com>
2015-05-02 21:18:35 +0300

    Release

--- lab5.py
+++ lab5.py
@@ -3,12 +3,14 @@ __author__ = 'fiodar'
 from boundary_problem import *
 import matplotlib.pyplot as plt
 
-p = lambda x: 1. / x
-q = lambda x: -3. * x
-f = lambda x: -5. / x
-a, b, n = 1., 3., 500
-conditions = ([5., -4., -2.], [3., 1., 0.])
+p = lambda x: -4 * x ** 2
+q = lambda x: -x ** 3
+f = lambda x: - 3 * x
+a, b, n = 1., 2., 500
+conditions = ([5., 2., -1.], [2., 1., 3.])
 y = ode_linear_2nd_order((p, q), (a, b), conditions, n, f)
 
 plt.plot(np.linspace(a, b, n), y)
+plt.xlim(1, 2)
+plt.grid()
 plt.show()
